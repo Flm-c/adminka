0c401e106cd6726c2a085e96a2620fec23f9af76
Daniil Uskov <clumsyfl@gmail.com>
2024-04-15 22:06:18 +0500

    Добавлены Routers

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const app = express();
-const fs = require("fs").promises;
 const path = require("path");
+
+const mainRouter = require("./routes/main");
+const gamesRouter = require("./routes/games");
 
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", async (req, res) => {
-  await fs.readFile("./public/index.html").then((data) => {
-    res.header("Content-Type", "text/html");
-    res.send(data);
-  });
-});
+app.use(
+  express.static(path.join(__dirname, "public")),
+  mainRouter,
+  gamesRouter
+);
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на ${PORT} порту`);
