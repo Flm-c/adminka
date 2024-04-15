fb360ba41f04da61e7fed5953a675d01ac685acf
Daniil Uskov <clumsyfl@gmail.com>
2024-04-15 21:37:51 +0500

    добавлен GET запрос по адресу "/"

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+
 const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Здарова!!!!!");
+});
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на ${PORT} порту`);
