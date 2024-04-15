519405903a43f84682d5f863ba839b224be73ecf
Daniil Uskov <clumsyfl@gmail.com>
2024-04-15 21:53:30 +0500

    добавлен фронтенд в папку public

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const fs = require("fs").promises;
+const path = require("path");
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Здарова!!!!!");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", async (req, res) => {
+  await fs.readFile("./public/index.html").then((data) => {
+    res.header("Content-Type", "text/html");
+    res.send(data);
+  });
 });
 
 app.listen(PORT, () => {
